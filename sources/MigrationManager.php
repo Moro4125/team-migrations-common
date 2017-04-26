0f3ec5d61fce9df3cfc4102e0fa42ad856bda460
Andrey Morozkin <andrey.dmitrievich@gmail.com>
2017-04-26 13:50:12 +0300

    Bugfix.

--- sources/MigrationManager.php
+++ sources/MigrationManager.php
@@ -24,7 +24,7 @@ use \ErrorException;
  */
 class MigrationManager implements SplSubject
 {
-	const VERSION = '1.6.6';
+	const VERSION = '1.6.7';
 
 	const EVENT_INIT_SERVICE           = 'team-migrations.init_service';
 	const EVENT_ASK_MIGRATION_LIST     = 'team-migrations.ask_migration_list';
